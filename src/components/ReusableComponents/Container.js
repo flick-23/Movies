1c516e959f0eaf9b8e80e006089261f38bc7f9ab
Venkatesh D <venkigd8@gmail.com>
2022-02-11 21:34:31 +0530

    Project setup

---
+++ src/components/ReusableComponents/Container.js
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const Container = ({ children }) => {
+  return (
+    <div css={styles} className="container">
+      {children}
+    </div>
+  );
+};
+
+const styles = css`
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+`;
+
+export default Container;
